a36ab60710579c8bfd80832363d4bd10ea97bd32
RuslaYupyn <yupynruslanevgenovich@gmail.com>
2016-06-21 22:35:21 +0300

    added extension functions for feature returning;

--- CRLAlomofireFeature/CRLAlomofireFeatureAPI.swift
+++ CRLAlomofireFeature/CRLAlomofireFeatureAPI.swift
@@ -7,8 +7,63 @@
 //
 
 import Foundation
+import Future
+import Alamofire
+//extension
 
-//extension 
+extension Alamofire.Request {
+    func responseJSON() -> Future<AnyObject> {
+        let p = Promise<AnyObject>()
+        self.responseJSON() { response in
+            switch response.result {
+            case .Success(let value):
+                p.trySuccess(value)
+            case .Failure(let error):
+                p.tryFail(error)
+            }
+        }
+        return p.future
+    }
+    
+    func response() -> Future<AnyObject> {
+        let p = Promise<AnyObject>()
+        self.response() { response in
+            switch response.result {
+            case .Success(let value):
+                p.trySuccess(value)
+            case .Failure(let error):
+                p.tryFail(error)
+            }
+        }
+        return p.future
+    }
+    
+    func responseString(encoding: NSStringEncoding) -> Future<AnyObject> {
+        let p = Promise<AnyObject>()
+        self.responseString(encoding) { response in
+            switch response.result {
+            case .Success(let value):
+                p.trySuccess(value)
+            case .Failure(let error):
+                p.tryFail(error)
+            }
+        }
+        return p.future
+    }
+    
+    func responsePropertyList(options: NSPropertyListReadOptions) -> Future<AnyObject> {
+        let p = Promise<AnyObject>()
+        self.responsePropertyList(options) { response in
+            switch response.result {
+            case .Success(let value):
+                p.trySuccess(value)
+            case .Failure(let error):
+                p.tryFail(error)
+            }
+        }
+        return p.future
+    }
+}
 
 class CRLAlomofireFeature {
     
--- CRLAlomofireFeatureTests/CRLAlomofireFeatureTests.swift
+++ CRLAlomofireFeatureTests/CRLAlomofireFeatureTests.swift
@@ -7,12 +7,13 @@
 //
 
 import XCTest
-@testable import CRLAlomofireFeature
+import CRLAlomofireFeature
 
 class CRLAlomofireFeatureTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        print("sutupped!!!")
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
