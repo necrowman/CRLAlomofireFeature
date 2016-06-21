741ec9928b90aa72830edc831ef23d5662a32bae
RuslaYupyn <yupynruslanevgenovich@gmail.com>
2016-06-21 21:58:15 +0300

    Project and dependencies set up

---
+++ CRLAlomofireFeature/CRLAlomofireFeatureAPI.swift
@@ -0,0 +1,15 @@
+//
+//  CRLAlomofireFeatureAPI.swift
+//  CRLAlomofireFeature
+//
+//  Created by Ruslan Yupyn on 6/21/16.
+//  Copyright © 2016 Crossroad Labs. All rights reserved.
+//
+
+import Foundation
+
+//extension 
+
+class CRLAlomofireFeature {
+    
+}
