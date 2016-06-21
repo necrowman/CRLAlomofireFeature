97e8499ab280ded0f7e9ccf56de52127284ff3fc
RuslaYupyn <yupynruslanevgenovich@gmail.com>
2016-06-21 20:11:06 +0300

    Initial Commit

---
+++ CRLAlomofireFeature/CRLAlomofireFeature.h
@@ -0,0 +1,19 @@
+//
+//  CRLAlomofireFeature.h
+//  CRLAlomofireFeature
+//
+//  Created by Ruslan Yupyn on 6/21/16.
+//  Copyright © 2016 Crossroad Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CRLAlomofireFeature.
+FOUNDATION_EXPORT double CRLAlomofireFeatureVersionNumber;
+
+//! Project version string for CRLAlomofireFeature.
+FOUNDATION_EXPORT const unsigned char CRLAlomofireFeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRLAlomofireFeature/PublicHeader.h>
+
+
