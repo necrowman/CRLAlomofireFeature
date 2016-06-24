be6131ea22c829557ecb486c5dc924c9edac5190
RuslaYupyn <yupynruslanevgenovich@gmail.com>
2016-06-24 16:29:18 +0300

    added testSimpleJSONStringParsing(); added testSimpleJSONServerNotFound();

--- CRLAlomofireFeatureTests/CRLAlomofireFeatureTests.swift
+++ CRLAlomofireFeatureTests/CRLAlomofireFeatureTests.swift
@@ -14,6 +14,9 @@ import CRLAlomofireFeature
 
 class CRLAlomofireFeatureTests: XCTestCase {
     
+    let correctURLBase = "https://raw.githubusercontent.com/necrowman/CRLAlomofireFeature/master/"
+    let uncorrectURLBase = "https://raw.githubusercontent12.com/necrowman/CRLAlomofireFeature/master/"
+    
     override func setUp() {
         super.setUp()
         
@@ -26,7 +29,7 @@ class CRLAlomofireFeatureTests: XCTestCase {
     
     func testSimpleString() {
         let asyncExpectation = expectationWithDescription("AlamofireResponseString")
-        let urlString = "https://raw.githubusercontent.com/necrowman/CRLAlomofireFeature/master/simpleTestURL.txt"
+        let urlString = "\(correctURLBase)simpleTestURL.txt"
         let future = Alamofire.request(.GET, urlString).responseString()
         future.onSuccess { value in
             print("RESULT VALUE -> ", value)
@@ -46,7 +49,7 @@ class CRLAlomofireFeatureTests: XCTestCase {
     
     func testSimpleStringServerNotFound() {
         let asyncExpectation = expectationWithDescription("AlamofireResponseString")
-        let urlString = "https://raw.githubusercontent12.com/necrowman/CRLAlomofireFeature/master/simpleTestURL.txt"
+        let urlString = "\(uncorrectURLBase)simpleTestURL.txt"
         let future = Alamofire.request(.GET, urlString).responseString()
         future.onSuccess { value in
             print("RESULT VALUE -> ", value)
@@ -65,45 +68,49 @@ class CRLAlomofireFeatureTests: XCTestCase {
         }
     }
     
-    func testSimpleJSON() {
-//        let asyncExpectation = expectationWithDescription("AlamofireResponseString")
-//        let urlString = "https://raw.githubusercontent.com/necrowman/CRLAlomofireFeature/master/simpleTestURL.txt"
-//        let future = Alamofire.request(.GET, urlString).responseString()
-//        future.onSuccess { value in
-//            print("RESULT VALUE -> ", value)
-//            XCTAssertEqual(value, "Test String")
-//            asyncExpectation.fulfill()
-//        }
-//        future.onFailure { error in
-//            print("ERROR VALUE -> ", error)
-//            XCTFail("Whoops, error \(error)")
-//            asyncExpectation.fulfill()
-//        }
-//        
-//        self.waitForExpectationsWithTimeout(5) { error in
-//            XCTAssertNil(error)
-//        }
+    func testSimpleJSONStringParsing() {
+        let asyncExpectation = expectationWithDescription("AlamofireResponseString")
+        let urlString = "\(correctURLBase)simpleJSONURL.json"
+        let future = Alamofire.request(.GET, urlString).responseJSON()
+        future.onSuccess { value in
+
+            let stringValue = value["testString"]
+            print("RESULT VALUE -> ", stringValue)
+            XCTAssertEqual(stringValue, "string value")
+            asyncExpectation.fulfill()
+        }
+        future.onFailure { error in
+            print("ERROR VALUE -> ", error)
+            XCTFail("Whoops, error \(error)")
+            asyncExpectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(5) { error in
+            XCTAssertNil(error)
+        }
     }
     
     func testSimpleJSONServerNotFound() {
-//        let asyncExpectation = expectationWithDescription("AlamofireResponseString")
-//        let urlString = "https://raw.githubusercontent12.com/necrowman/CRLAlomofireFeature/master/simpleTestURL.txt"
-//        let future = Alamofire.request(.GET, urlString).responseString()
-//        future.onSuccess { value in
-//            print("RESULT VALUE -> ", value)
-//            XCTFail("Whoops, error \(value)")
-//            XCTAssertEqual(value, "Test String")
-//            asyncExpectation.fulfill()
-//        }
-//        future.onFailure { error in
-//            print("ERROR VALUE -> ", error)
-//            XCTAssertNotNil(error)
-//            asyncExpectation.fulfill()
-//        }
-//        
-//        self.waitForExpectationsWithTimeout(5) { error in
-//            XCTAssertNil(error)
-//        }
+        let asyncExpectation = expectationWithDescription("AlamofireResponseString")
+        let urlString = "\(uncorrectURLBase)simpleJSONURL.json"
+        let future = Alamofire.request(.GET, urlString).responseJSON()
+        future.onSuccess { value in
+            
+            let stringValue = value["testString"]
+            print("RESULT VALUE -> ", stringValue)
+            XCTFail("Whoops, error \(stringValue)")
+            XCTAssertEqual(stringValue, "string value")
+            asyncExpectation.fulfill()
+        }
+        future.onFailure { error in
+            print("ERROR VALUE -> ", error)
+            XCTAssertNotNil(error)
+            asyncExpectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(5) { error in
+            XCTAssertNil(error)
+        }
     }
     
     func testPerformanceExample() {
